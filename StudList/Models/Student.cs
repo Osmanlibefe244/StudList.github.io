d4caefb9b2e7869f5c14cc2f1be3064ef96a834a
Osmanlibefe244 <99293519+Osmanlibefe244@users.noreply.github.com>
2022-02-13 02:46:34 +0300

    Add files via upload

---
+++ StudList/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace StudList.Models
+{
+    public class Student
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string MidName { get; set; }
+        public int Grade { get; set; }    
+
+        public int SubjectId { get; set; }
+        public Subject Subject { get; set; }
+    }
+}
