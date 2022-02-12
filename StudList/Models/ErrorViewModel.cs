120d9a01da50b0073cf8976db84683e0adc945dc
Osmanlibefe244 <99293519+Osmanlibefe244@users.noreply.github.com>
2022-02-13 02:52:33 +0300

    Delete StudList directory

--- StudList/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StudList.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- StudList/Models/Subject.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StudList.Models
-{
-    public class Subject
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public List<Student> Students { get; set; } = new List<Student>();
-    }
-}
